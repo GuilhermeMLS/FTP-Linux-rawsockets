6df28f3c36b6615480ff5122117e32a964e9298b
GuilhermeMLS <guitaikion@gmail.com>
2019-05-14 14:44:17 -0300

    Primeiro commit

---
+++ cmake-build-debug/CMakeFiles/trab1_redes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trab1_redes.dir/main.c.o"
+  "trab1_redes.pdb"
+  "trab1_redes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trab1_redes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/trab1_redes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trab1_redes.
+# This may be replaced when dependencies are built.
