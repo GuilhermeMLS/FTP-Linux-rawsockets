c069a789fd38b5541747b40bdb8e8da03675b562
GuilhermeMLS <guitaikion@gmail.com>
2019-06-15 16:52:29 -0300

    adiciona conexao no messages

--- messages.c
+++ messages.c
@@ -83,7 +83,7 @@ int checkCRC(message *msg)
         printf("\x1B[31mCRC Error\x1B[0m: received=%d, calculated =%d\n", crc, rCrc);
     }
 #endif
-    return (crc == rCrc);c
+    return (crc == rCrc);
 }
 
 // TODO: justificar o timeOut escolhido depois; verificar o funcionamento desta função
